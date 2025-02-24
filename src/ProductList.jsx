027588f41ae6b10beee9a930661b185f1b4f5808
ciengrantz <rhysgangaram213@gmail.com>
2025-02-24 06:36:07 -0500

    task 1 modification

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,11 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import additem from './CartSlice.jsx';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const [addedtoCart, setaddedtoCart] = useState({})
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -268,6 +269,22 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
+                    {plantsArray.map((category, index) => (
+        <div key={index}>
+            <h1><div>{category.category}</div></h1>
+            <div className="product-list">
+                {category.plants.map((plant, plantIndex) => (
+                <div className="product-card" key={plantIndex}>
+                    <img className="product-image" src={plant.image} alt={plant.name} />
+                    <div className="product-title">{plant.name}</div>
+                    
+                    <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                </div>
+                ))}
+            </div>
+        </div>
+        ))}
+            
 
 
         </div>
